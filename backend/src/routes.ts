93248a91929daa07f00ef221009bbcd55f65e117
ArmandoAAJ <armando-a-j@hotmail.com>
2020-10-14 10:58:29 -0300

    Create API
    Criação da api com as funcionalidade de cadastrar orfanato e listar.

---
+++ backend/src/routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import multer from 'multer';
+import OrphanagesController from './controllers/OrphanagesController';
+
+import uploadCconfig from './config/upload';
+const routes = Router();
+
+const upload = multer(uploadCconfig);
+
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
+
+
+
+export default routes;
